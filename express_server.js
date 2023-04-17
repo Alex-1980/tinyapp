7be19d5b9219460457c59c4eb1b59717f95121df
Chansoo Han <devhan1207@gmail.com>
2023-04-17 13:24:00 -0400

    added paramsId

--- express_server.js
+++ express_server.js
@@ -24,6 +24,7 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
+
 });
 
 app.get("/fetch", (req, res) => {
@@ -33,6 +34,12 @@ app.get("/fetch", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const paramsId = req.params.id;
+  const templateVars = { id: paramsId, longURL: urlDatabase[paramsId] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
