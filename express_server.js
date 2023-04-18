25d3098aade96e5f684e54cf2d900acb26e3cc9b
Chansoo Han <devhan1207@gmail.com>
2023-04-17 20:45:38 -0400

    added post request

--- express_server.js
+++ express_server.js
@@ -4,6 +4,12 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {}
+
+generateRandomString();
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -36,11 +42,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const paramsId = req.params.id;
   const templateVars = { id: paramsId, longURL: urlDatabase[paramsId] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
