ea6716a3b72f60aed91608a842e1a3b9ead69294
Chansoo Han <devhan1207@gmail.com>
2023-04-18 20:59:43 -0400

    added cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
-const cookieSession = require('cookie-session')
+// const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 const express = require("express");
 
 const app = express();
@@ -6,10 +7,12 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ["key1", "key2"]
-}))
+// app.use(cookieSession({
+//   name: 'session',
+//   keys: ["key1", "key2"]
+// }));
+
+app.use(cookieParser())
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -28,40 +31,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies.username,
+    urls: urlDatabase
+  };
+  console.log(req.cookies.username)
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const username = req.cookies.username;
+  const templateVars = { username };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const paramsId = req.params.id;
-  const templateVars = { id: paramsId, longURL: urlDatabase[paramsId] };
+  const username = req.cookies.username;
+  const templateVars = { id: paramsId, longURL: urlDatabase[paramsId], username };
   res.render("urls_show", templateVars);
 });
 
@@ -92,10 +80,14 @@ app.post("/urls/:id/edit", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  req.session.userID = username;
-  console.log(req.session.username)
-  res.redirect("/urls")
-})
+  res.cookie("username", username); // store item in cookie using cookie-parser ("key", value)
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
